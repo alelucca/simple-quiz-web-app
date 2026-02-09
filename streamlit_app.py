302c43f67e480a98a899b306db6499620528d154
Lucca Alessandro <z.alessandro.lucca@guest.rcs.it>
2026-02-09 15:42:26 +0100

    avoid caching error

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,23 +73,21 @@ def init_session_state():
         st.session_state.feedback_message = None
 
 
-@st.cache_resource
-def init_connection():
-    return pymongo.MongoClient(host=st.secrets["mongo"]["mongodb_uri"])
-
 @st.cache_data
-def get_data(client: pymongo.MongoClient):
-    db = client.quiz_app
+def get_data():
+    _client = pymongo.MongoClient(host=st.secrets["mongo"]["mongodb_uri"])
+    db = _client.quiz_app
     # items = db.infermieristica.find({},{"materia":1,"lista_domande_risposte":1})
     items = db.infermieristica.find()
 
     items = list(items)  # make hashable for st.cache_data
     
-    os.makedirs("QUIZ_CLEAN/JSON", exist_ok=True)
+    folder = "QUIZ_CLEAN/JSON"
+    os.makedirs(folder, exist_ok=True)
     for item in items:
         filename=item["materia"]
         lista_domande_risposte=item["lista_domande_risposte"]
-        with open(f"QUIZ_CLEAN/JSON/{filename}_final.json", "w", encoding="utf-8") as f:
+        with open(f"{folder}/{filename}_final.json", "w", encoding="utf-8") as f:
             json.dump(lista_domande_risposte, f, indent=2, ensure_ascii=False)
 
 # ============================================================================
@@ -1109,11 +1107,9 @@ def main():
     
     # Inizializza session state
     init_session_state()
-
-    # Inizializza il client mongodb
-    mongodb_client = init_connection()
-    # Ottieni la lista di quiz dal client e salvala in locale
-    get_data(client=mongodb_client)
+    
+    # Ottieni la lista di quiz dal client mongodb e salvala in locale
+    get_data()
     
     # Sidebar con logout
     with st.sidebar:
